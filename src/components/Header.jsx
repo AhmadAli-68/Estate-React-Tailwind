58981bd3cf88da5531a8f81f0bdfa80444fd2fbc
Ahmad Ali <ahmad.ali619x@gmail.com>
2026-09-12 19:23:48 +0500

    Implement the Header section and mobile menu toggle in the Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
+import Navbar from './Navbar'
+
 const Header = () => {
   return (
-    <div>
-      
+    <div
+      id='header'
+      className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'
+      style={{ backgroundImage: "url('/header_img.png')" }}
+    >
+      <Navbar />
+
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+        <h2 className='text-5xl font-semibold sm:text-6xl md:text-[82px] inline-block max-w-3xl pt-20'>Explore homes that fit your dreams</h2>
+
+        <div className='space-x-6 mt-16'>
+          <a
+            href='#projects'
+            className='border border-white rounded-lg px-8 py-3 hover:bg-white hover:text-black transition-all'
+          >
+            Projects
+          </a>
+          <a
+            href='#contact'
+            className='bg-blue-600 rounded-lg px-8 py-3 hover:bg-blue-700 transition-all'
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    }
+  }, [showMobileMenu])
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -20,6 +35,39 @@ const Navbar = () => {
         </ul>
 
         <button className='hidden md:block bg-white px-8 py-2 rounded-full cursor-pointer'>Sign up</button>
+
+        <img
+          src={assets.menu_icon}
+          alt='menu icon'
+          className='md:hidden w-7 cursor-pointer'
+          onClick={() => setShowMobileMenu(true)}
+        />
+      </div>
+
+      {/* ---- Mobile Menu ---- */}
+      <div className={`fixed w-full md:hidden ${showMobileMenu ? 'translate-x-0' : 'translate-x-full'} right-0 top-0 bottom-0 overflow-hidden bg-black text-white transition-all duration-300 ease-in-out`}>
+
+        <div className='flex justify-end p-6'>
+          <img
+            src={assets.cross_icon}
+            className='w-8 cursor-pointer bg-white rounded-lg p-2'
+            alt='cross-icon'
+            onClick={() => setShowMobileMenu(false)}
+          />
+        </div>
+
+        <ul className='flex flex-col justify-end items-start gap-2 mt-5 px-5 text-lg font-medium'>
+          {navLinks.map(({ id, link, title }) => (
+            <a
+              key={id}
+              href={link}
+              className='text-center px-4 py-2 rounded-lg inline-block hover:bg-gray-500/20 w-full transition-all ease-in-out duration-300'
+              onClick={() => setShowMobileMenu(false)}
+            >
+              {title}
+            </a>
+          ))}
+        </ul>
       </div>
     </div>
   )
